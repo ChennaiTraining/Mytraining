1ced51c15d6f2f037787aef36779df29e7941dc6
ChennaiTraining <praveenkumarsridhar2488@gmail.com>
2022-02-01 23:08:24 +0530

    23.38

--- src/org/gitproject/GitTest.java
+++ src/org/gitproject/GitTest.java
@@ -3,7 +3,7 @@ package org.gitproject;
 public class GitTest {
 	
 	public void projectGit() {
-		System.out.println("GIT "+ 10);
+		System.out.println("GIT\n\n"+ 100);
 		
 	}
 	public static void main(String[] args) {
