fa07825cc13ecc304f6d1f33456cc7dfde490232
ChennaiTraining <praveenkumarsridhar2488@gmail.com>
2022-02-02 11:03:57 +0530

    02-02/11:03

--- src/org/gitproject/GitTest.java
+++ src/org/gitproject/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 	
 	public void projectGit() {
 		System.out.println("GIT "+ 10);
+		System.out.println("MIT\n" +20);
+		System.out.println("NIT\t"+ 30);
 		
 	}
 	public static void main(String[] args) {
